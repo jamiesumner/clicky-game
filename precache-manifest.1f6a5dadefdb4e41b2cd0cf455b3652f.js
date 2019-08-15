807ee75b241f4c72bd009f63920ddd2fde25a781
Jamie Sumner <jamiesumner@Jamies-MacBook-Air.local>
2019-08-15 18:14:58 -0400

    Updates

---
+++ precache-manifest.1f6a5dadefdb4e41b2cd0cf455b3652f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eaa33544267c622c556c694ae4702f1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "2159595c36b22535e44f",
+    "url": "/clicky-game/static/js/main.a9502e55.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
